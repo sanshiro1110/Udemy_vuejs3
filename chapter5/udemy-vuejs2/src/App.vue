130e21026855edaeb866a94e360dbda58f446e66
sanshiro1110 <sanshirou1110_0508@yahoo.co.jp>
2020-10-08 23:32:34 +0900

    vuejs component & v-model

--- chapter5/udemy-vuejs2/src/App.vue
+++ chapter5/udemy-vuejs2/src/App.vue
@@ -1,28 +1,103 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <LikeHeader>
+      <h3>はじめまして</h3>
+    </LikeHeader>
+    <LikeNumber :total-number="number" @my-click="number = $event"></LikeNumber>
+    <button @click="currentComponent = 'Home'">Home</button>
+    <button @click="currentComponent = 'About'">About</button>
+    <!-- <About v-if="currentComponent === 'About'"></About>
+    <Home v-if="currentComponent === 'Home'"></Home> -->
+    <keep-alive>
+      <component :is="currentComponent"></component>
+    </keep-alive>
+    <div>
+      <h2>イベントのホーム</h2>
+
+      <label for="title">タイトル</label>
+      <input
+        type="text"
+        id="title"
+        v-model.lazy="eventData.title"
+      >
+      <p>{{ eventData.title }}</p>
+
+      <label for="maxNumber">最大人数</label>
+      <input
+        type="number"
+        id="maxNumber"
+        v-model.number="eventData.maxNumber"
+      >
+      <p>{{ eventData.maxNumber }}</p>
+
+      <label for="host">主催者</label>
+      <input
+        type="text"
+        id="host"
+        v-model.trim="eventData.host"
+      >
+      <p style="white-space: pre;">{{ eventData.host }}</p>
+
+      <label for="detail">イベントの内容</label>
+      <textarea id="detail" cols="30" rows="10" v-model="eventData.detail"></textarea>
+      <pre>{{ eventData.detail }}</pre>
+
+      <label for="isPrivate">非公開</label>
+      <input type="checkbox" id="isPrivate" v-model="eventData.isPrivate">
+      <p>{{ eventData.isPrivate }}</p>
+
+      <p>参加条件</p>
+      <input
+        type="checkbox"
+        id="10"
+        value="10代"
+        v-model="eventData.target"
+      >
+      <label for="10">10代</label>
+      <input
+        type="checkbox"
+        id="20"
+        value="20代"
+        v-model="eventData.target"
+      >
+      <label for="20">20代</label>
+      <input
+        type="checkbox"
+        id="30"
+        value="30代"
+        v-model="eventData.target"
+      >
+      <label for="30">30代</label>
+      <p>{{ eventData.target }}</p>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import LikeHeader from './components/LikeHeader.vue'
+import About from './components/About.vue'
+import Home from './components/Home.vue'
 
 export default {
-  name: 'App',
+  data() {
+    return {
+      number: 14,
+      currentComponent: "Home",
+      eventData: {
+        title: 'タイトル',
+        maxNumber: 0,
+        host: "",
+        detail: "",
+        isPrivate: false,
+        target: [],
+      }
+    };
+  },
   components: {
-    HelloWorld
+    LikeHeader,
+    About,
+    Home
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
